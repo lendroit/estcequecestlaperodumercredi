b5ee24bc3be8ba4738279c73dba13b84cc6af717
Sammy Teillet <sammy@MacBook-Pro-de-Sammy.local>
2019-04-06 14:19:10 +0200

    Updates

---
+++ precache-manifest.c769543bcba69cf7518dc880782c4a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0754b09a919918c5a865",
+    "url": "/estcequecestlaperodumercredi/static/css/main.8d4acf6f.chunk.css"
+  },
+  {
+    "revision": "0754b09a919918c5a865",
+    "url": "/estcequecestlaperodumercredi/static/js/main.67fe6011.chunk.js"
+  },
+  {
+    "revision": "cd1d8d614539fd711c44",
+    "url": "/estcequecestlaperodumercredi/static/js/runtime~main.5d1f5897.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/estcequecestlaperodumercredi/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "81bc604f4efe9e31eae80fa77c6a4af7",
+    "url": "/estcequecestlaperodumercredi/index.html"
+  }
+];
